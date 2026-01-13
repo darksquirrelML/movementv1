a35ad072df1ab8cd875fc033543bf6e12f406ca6
darksquirrelML <107252023+darksquirrelML@users.noreply.github.com>
2026-01-13 16:18:45 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import streamlit as st
+
+st.set_page_config(
+    page_title="Vehicle Movement Dashboard",
+    layout="wide"
+)
+
+st.title("🚚 Vehicle Movement Dashboard")
+
+st.markdown("""
+Please use the **sidebar** to navigate:
+- 🚐 Pick-up Lorry
+- 🚛 Tipper Truck
+- 🏗 Machinery
+""")
+
